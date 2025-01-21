9320d1728507caf41ea29fbf9168c2b3c28ea153
JardiWester <jj.wester102@gmail.com>
2025-01-21 13:04:34 +0100

    dsffsfsafsadfsa
    good description

--- Assets/Scripts/hapticButton.cs
+++ Assets/Scripts/hapticButton.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class hapticButton : hapticsPlayerParent
 {
+    [SerializeField] hapticPattern hoverpattern;
     public void buttonPressed()
     {
         playPattern();
+    }
+
+    public void hover()
+    {
+        playPattern(hoverpattern);
     }
 
 }
